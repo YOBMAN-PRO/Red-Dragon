2167106ac623a0a7e08ae115aa911ca9bf52d484
YOBMAN-PRO <109598933+YOBMAN-PRO@users.noreply.github.com>
2022-08-29 18:06:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
 
-global.alivemsg = `📤I am alive now😇
+عالمي . alivemsg  =  "أنا  متصل كيف يمكنني مساعدتك ❤️❤️"
 🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
 
 
@@ -29,8 +29,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['0777765558'] // owner number
+global.ownername = "😎𝙆.AYOUB😎❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
+global.owner = [0777765558']
+global.ownertag = '0777765558' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
-global.dragonemoji = '🧩' // Menu emoji
+عالمي . المؤلف  =  "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉"  // مؤلف الملصق
+عالمي . dragonemoji  =  '🧩'  // قائمة الرموز التعبيرية7
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['0777765558'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
